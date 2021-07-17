cc03d4947068b2eecdc1fcc1342ac4d79570fb6c
Tsaritsyn <tsarmihail2013@yandex.ru>
2021-07-17 17:15:55 +0200

    Implemented first test for sets

--- test.c
+++ test.c
@@ -3,9 +3,15 @@
 //
 
 #include "tests/test_bs_tree.h"
+#include "tests/test_set_long.h"
 
 
 void full_tree_test_set() {
+    if (!DEBUG_CONF) {
+        printf("Error in full_tree_test_set: it is not the debug configuration, tests will have no effect.\n");
+        return;
+    }
+
     print_tree_size();
     test_node_creation();
     test_find_node();
@@ -26,15 +32,30 @@ void full_tree_test_set() {
     test_merging_balanced_trees();
 }
 
+
+int full_set_testset() {
+    unsigned int num_passed = 0, num_total = 0;
+    int passed = 1;
+
+    printf("\nTEST SERIES FOR SET CONTAINER STARTED.\n");
+
+    int current_res = test_set_creation();
+    num_passed += current_res;
+    passed &= current_res;
+    num_total++;
+
+    printf("\n%u/%u tests passed.\n", num_passed, num_total);
+    printf("SET TEST SERIES %s.\n", (passed == 1) ? "PASSED" : "FAILED");
+    return passed;
+}
+
+
 // CHECK THAT YOU USE THE DEBUG CONFIGURATION BEFORE RUNNING THESE TESTS!
 // Since assert() statements do nothing in release configuration, these tests will always pass in this case.
 int main() {
-    if (!DEBUG_CONF) {
-        printf("Error: it is not the debug configuration, tests will have no effect.\n");
-        return -1;
-    }
 
-    full_tree_test_set();
+//    full_tree_test_set();
+    full_set_testset();
 
     return 0;
 }
---
+++ tests/test_set_long.h
@@ -0,0 +1,44 @@
+//
+// Created by Mikhail Tsaritsyn on 17.07.2021.
+//
+
+#ifndef MY_CLIB_TEST_SET_LONG_H
+#define MY_CLIB_TEST_SET_LONG_H
+
+
+#include "../data_structures/set_long.h"
+
+
+int test_set_creation() {
+
+    printf("\nSet creation test started.\n");
+
+    Set_long_ptr set = create_set();
+    printf("Empty set created, its size %lu bytes.\n", sizeof(Set_long));
+
+    unsigned int checks_passed = 0;
+    unsigned int checks_total = 0;
+    if (set->tree == NULL)
+        checks_passed++;
+    else
+        printf("Check 1 failed: set->tree != NULL.\n");
+    checks_total++;
+
+    if (set->size == 0)
+        checks_passed++;
+    else
+        printf("Check 2 failed: set->size != 0, but %lu.\n", set->size);
+    checks_total++;
+
+    if (checks_passed == checks_total) {
+        printf("Test passed.\n");
+        return 1;
+    }
+    else {
+        printf("Test failed (%u/%u checks passed).", checks_passed, checks_total);
+        return 0;
+    }
+}
+
+
+#endif //MY_CLIB_TEST_SET_LONG_H
